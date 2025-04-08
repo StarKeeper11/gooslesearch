fabd7d3aafb0cd59a637ac18e83ea897c06542c3
StarKeeper11 <iakuninvova11@gmail.com>
2025-04-07 20:10:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlitecloud
 
 try:
-    conn = sqlitecloud.connect("sqlitecloud://cdwkovcahz.g6.sqlite.cloud:8860/goosle?apikey=jPOcAQ1fGUqluIsAUbpEKtMjrPfZxxz3yg8V2IGTEIk")
+    conn = sqlitecloud.connect("sqlitecloud://cdwkovcahz.sqlite.cloud:8860/goosle?apikey=jPOcAQ1fGUqluIsAUbpEKtMjrPfZxxz3yg8V2IGTEIk")
     print("Connection successful!")
     conn.close()
 except Exception as e:
