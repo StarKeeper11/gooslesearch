ce011a416cc6a400e1d885794525d21a7ade1227
StarKeeper11 <iakuninvova11@gmail.com>
2025-04-16 22:29:13 +0000

    yay

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import *
 from google.cloud.firestore_v1.base_query import FieldFilter
+from flask import *
+from flask_cors import CORS
 
 # Use a service account.
 cred = credentials.Certificate('service-account.json')
@@ -19,3 +21,16 @@ def doesUserExist(username):
         return True
     return False
 
+def getUsername(id):
+    query = user_ref.document(str(id))
+    if query.get().exists:
+        return query.get().to_dict()['username']
+    else:
+        return None
+
+app = Flask(__name__)
+
+@app.route("/getusername", methods=["POST"])
+def getUsernameRoute():
+    return True
+print(getUsername(1))
