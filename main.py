2cbbb07826efcd3c6cd20f52ac9d92ea5853f99c
StarKeeper11 <iakuninvova11@gmail.com>
2025-04-16 18:43:10 +0000

    I have done it

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-##use cloud firestore!
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import *
+from google.cloud.firestore_v1.base_query import FieldFilter
+
+# Use a service account.
+cred = credentials.Certificate('service-account.json')
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+user_ref = db.collection('users')
+
+def doesUserExist(username):
+    query = user_ref.where(filter=FieldFilter('username', '==', username)).stream()
+    for doc in query:
+        return True
+    return False
+
